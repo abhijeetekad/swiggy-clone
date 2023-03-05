160916d6f488ff1947e1d17a6304705ed90aec8a
abhijeetekad <ekadabhijeet4@gmail.com>
2023-03-05 18:36:24 +0530

    header initial setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import logo from "./Media/images.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">
+          <img src={logo} />
+        </a>
+
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </header>
+      <body className="App-body">efr</body>
+      <footer className="App-footer">sdf</footer>
+    </div>
+  );
+}
+
+export default App;
